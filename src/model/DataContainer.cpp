de5747260662be81fd8a1265e14e38a07979d3fe
tom1452 <tautret142005@gmail.com>
2026-05-19 14:32:20 +0200

    imple datacobtainer

---
+++ src/model/DataContainer.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+                           DataContainer  -  description
+                             -------------------
+    début                : 19/05/2026
+    copyright            : (C) 2026 par tautret
+    e-mail               : tom.autret@insa-lyon.fr
+*************************************************************************/
+
+//---------- Réalisation de la classe <DataContainer> (fichier DataContainer.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "DataContainer.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+//----------------------------------------------------- Méthodes publiques
+
+void DataContainer::addAttribute(const Attribute*)
+{
+    
+}
